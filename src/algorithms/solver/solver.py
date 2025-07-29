44695e93679dd0262063a372c8e58adb26b9cfbc
antonioaalves <antonio.alves@tlantic.com>
2025-07-29 18:26:00 +0100

    Changes to algorithm solver params

--- src/algorithms/solver/solver.py
+++ src/algorithms/solver/solver.py
@@ -124,7 +124,7 @@ def solve(
 
         # Use only verified OR-Tools parameters
         solver.parameters.num_search_workers = 1
-        solver.parameters.max_time_in_seconds = 30  # Short timeout for testing
+        solver.parameters.max_time_in_seconds = 600  # Very important to set it high to avoid timeout
 
         logger.info("Attempting solve with verified parameters...")
 
