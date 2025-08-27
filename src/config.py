5c619a60036a148f215e30657e623feb96e67be5
antonioaalves <antonio.alves@tlantic.com>
2025-08-27 16:53:27 +0100

    Insert results set to true

--- src/config.py
+++ src/config.py
@@ -183,7 +183,7 @@ CONFIG = {
                 #    'parameters': {}
                 #},
                 'insertions': {
-                    'insert_results': False
+                    'insert_results': True
                 }
             },
             'substages': {
